197518ce2233bd9270351834d7c53ecc953df9e0
kitan <alabiridolakitan487@gmail.com>
2024-12-11 12:19:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+var developer = 'Kitan'
+var designer = 'Banjo'
+var accountant = 'Sydney'
+
+// concatenation
+var sentence = 'i have a developer name {mayor} and he got two friends named {dev} and {dev}'
+console.log(sentence);
+console.log(sentence.length);
+console.log(sentence.toUpperCase());
+console.log(sentence.toLowerCase());
+
+var age = 17
+if(age >= 18){
+    console.log('you can vote');
+}else{
+    console.log('you are too young to vote');
+}
+
+var student = false
+if(student == true){
+    console.log('free ticket');
+}else{
+    console.log('pay your complete money');
+}
+
+var me = 'tired'
+if(me != 'tired'){
+    console.log('continue working');
+}else{
+    console.log('take a break');
+}
+
+var presido = 'Tpain'
+if(presido != 'Tpain'){
+    console.log('life is good');
+}else{
+    console.log('sapa dey for everybody');
+}
+
+var job = 'police'
+if(job == 'lawyer'){
+    console.log('your bill is 200k');
+}else if(job == 'police'){
+    console.log("your bill is 1 million");
+}else if(job == 'influencer'){
+    console.log('your bill is 600k');
+}else if(job == 'instructor'){
+    console.log('your bill is 50k');
+}else{
+    console.log('your bill is 100k')
+}
+
+
+
+
